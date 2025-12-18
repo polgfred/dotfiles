99d2e7d50a80dfa457950e33abbe390821e107cc
Frederick Polgardy <fred@polgardy.com>
2025-12-18 19:25:47 +0000

    keep docker-compose command around

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# remove docker-compose v1 so oh-my-zsh uses v2
-if [ -f /etc/alternatives/docker-compose ]; then
-  sudo update-alternatives --remove-all docker-compose
-else
-  sudo rm -f /usr/local/bin/docker-compose
-fi
-
 # update bash shells to zsh
 cp /etc/passwd ~/.passwd
 sudo sed -i 's/\/bash/\/zsh/' /etc/passwd
