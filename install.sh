a96ff46646f9a363e54de56e0e4e9805f4ef6877
Frederick Polgardy <fred@rifflecm.com>
2026-01-22 16:24:17 +0000

    history file for riff console

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo sed -i 's/\/bash/\/zsh/' /etc/passwd
 # link some files
 ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.npmrc ~/.npmrc
+ln -sf $PWD/.riff_history ~/.riff_history
 ln -sf $PWD/fred.zsh-theme ~/.oh-my-zsh/custom/themes/fred.zsh-theme
 
 # include this in our container gitconfig
